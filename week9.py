9c5db6d667a7eb8f6c33ce637691470cdfa031ee
jonathan mulyono <139569376+jomul-ai@users.noreply.github.com>
2024-11-20 16:22:53 +0700

    Add files via upload

---
+++ week9.py
@@ -0,0 +1,34 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1200,720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() /2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen. fill('purple')
+
+    pygame.draw.circle(screen, 'red', player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 200 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 200 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 200 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 200 * dt
+
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
